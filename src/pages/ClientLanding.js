56224f340331b9ee52b0b1f7f0f8061a648fe8c5
Grant Bowlds <grant.f.bowlds@vanderbilt.edu>
2022-03-29 17:50:08 -0500

    Add some things

--- src/pages/ClientLanding.js
+++ src/pages/ClientLanding.js
@@ -2,9 +2,9 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
+import { createTheme } from '@mui/material/styles';
 
 class ClientLanding extends React.Component {
-
     render(){
         return(
             <div>
@@ -12,15 +12,24 @@ class ClientLanding extends React.Component {
                 <br></br>
                 <div style={{
                     display: 'flex',
+                    alignItems: 'Justify',
+                    justifyContent: 'Left',
+                }}>
+                    <p style={{color:"#ffffff"}}>KOACORE is a full-scale safety management firm. As leaders in event management and COVID safety, we partner with best-in-class technology platforms, manufacturers, medical advisors, and staffing companies to provide a one-stop-shop for all safety in a Covid and post-Covid environment. KOACORE is proud to be part of the Return to Live by ensuring our clients have access to best-in-class safety, guidance, equipment, and support.</p>
+                </div>
+                <div style={{
+                    display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
+                    padding: 10,
                 }}>
-                    <Button component={Link} to="/trade">
-                    Trade
-                    </Button>
-                    <Button component={Link} to="/client-survey">
-                    Events
-                    </Button>
+                        <Button variant="contained" component={Link} to="/trade">
+                        I need to source supplies
+                        </Button>
+                        
+                        <Button variant="contained" component={Link} to="/client-survey">
+                        I'm looking for safety management for my event
+                        </Button>
                 </div>
             </div>
         );
