2eecae3b95cefc032a459d8e45523416d7b479a7
Grant Bowlds <grant.f.bowlds@vanderbilt.edu>
2022-03-30 14:34:26 -0500

    add a validation

---
+++ src/AirtableScripts/TestCountingScript.js
@@ -0,0 +1,36 @@
+// This script is for
+let table_1 = base.getTable('Master List');
+let tests = base.getTable("Attendees");
+let notCounted = tests.getView("Not Counted attendees");
+let table = table_1.getView("Grant's Test Counting");
+let result = await table.selectRecordsAsync();
+let attendee = await notCounted.selectRecordsAsync();
+let runningTotal;
+let runningTotalwRes;
+let meetsConditions;
+
+for (let record of result.records) {
+    runningTotal = record.getCellValue("Rapid Antigen Tests Given");
+    runningTotalwRes = record.getCellValue("Tests w/ a Reservation");
+    meetsConditions = attendee.records.filter((potentialEvent) => {
+        if (potentialEvent.getCellValue("Event Lookup") === record.getCellValue("Event Key")) {
+            // if the names match, return true
+            return true;
+        }
+        return false;
+    });
+    for (let record1 of meetsConditions) {
+        runningTotal += record1.getCellValue('Number of attendees needing test');
+        if (record1.getCellValue("Pre-registered Test") == 1) {
+            runningTotalwRes += record1.getCellValue('Number of attendees needing test');
+        }
+        await tests.updateRecordAsync(record1, {
+        'Counted': true
+    });
+    }
+    let recordId = record.id
+    await table_1.updateRecordAsync(recordId, {
+        'Rapid Antigen Tests Given': runningTotal,
+        'Tests w/ a Reservation': runningTotalwRes
+    });
+ }
