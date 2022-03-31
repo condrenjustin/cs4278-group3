47bfd53e0d64e019b08c1db93b6530b8e9c8043e
Grant Bowlds <grant.f.bowlds@vanderbilt.edu>
2022-03-31 12:14:33 -0500

    Add trade landing page

--- src/pages/TradeLanding.js
+++ src/pages/TradeLanding.js
@@ -4,6 +4,11 @@ import NavBar from '../components/NavBar';
 /**
  * The trade landing page ... 
  */
+ class SurveyTrade extends React.Component {
+    render() {
+        /** Client survey component imported from airtable */
+    return <iframe class="airtable-embed" src="https://airtable.com/embed/shrpyuUQUkvmagP8j?backgroundColor=purple" width={1000} height={533} ></iframe>    }
+  }
 
 class TradeLanding extends React.Component {
 
@@ -12,12 +17,13 @@ class TradeLanding extends React.Component {
             <div>
                 <NavBar />
                 <br></br>
-                <h1 style={{color:"#ffffff"}}>Trade Landing</h1>
+                <h1 style={{color:"#ffffff"}}>Trade Form</h1>
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
                 }}>
+                    <SurveyTrade />;
                 </div>
             </div>
         );
