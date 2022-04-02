f3b00f54c013e9581c407abc3823d0a4dba7c9bf
Grant Bowlds <grant.f.bowlds@vanderbilt.edu>
2022-04-02 14:46:55 -0500

    Broken data dashboard

--- src/pages/DataDashboard.js
+++ src/pages/DataDashboard.js
@@ -2,34 +2,37 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Airtable from 'airtable';
 /** The data dashboard page ... */
-let venues, count;
 
-// Airtable api call to get most recent entry
-function getLocation(){
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: 'keypmQe98NRiZBQcw'}).base('appECiA8jbyRdHrbu');
-    base('CRM-Prospects').select({
-        // Selecting the first records in Contacts:
-        maxRecords: 1,
-        view: "Retrieve Most Recent"
-    }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
 
-        records.forEach(function(record) {
-            venues = record.get("Venues");
-            count = record.get("Test Count");
-        });
-
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
-}
 
 
 
 class DataDashboard extends React.Component {
-    getLocation();
     render(){
+        var x, y;
+        // Airtable api call to get most recent entry
+
+        var Airtable = require('airtable');
+        var base = new Airtable({apiKey: 'keypmQe98NRiZBQcw'}).base('appECiA8jbyRdHrbu');
+        base('CRM-Prospects').select({
+            maxRecords: 1,
+            view: 'Retrieve Most Recent'}).firstPage(function(err, records) {
+                if (err) { console.error(err); return; }
+                records.forEach(function(record) {
+                    x = record.get('Venues');
+                });
+            });
+            
+        base('CRM-Prospects').select({
+            maxRecords: 1,
+            view: 'Retrieve Most Recent'}).firstPage(function(err, records) {
+                if (err) { console.error(err); return; }
+                records.forEach(function(record) {
+                    y = record.get('Test Count');
+                });
+            });
+        
+
         return(
             <div>
                 <NavBar />
@@ -40,7 +43,8 @@ class DataDashboard extends React.Component {
                     alignItems: 'center',
                     justifyContent: 'center',
                 }}>
-                    <p style={{color:"#ffffff"}}>{"Thank you for considering KOACORE for your safety needs. We have worked with events ranging from Elton John to Coldplay, and we cannot wait to add you to the KOA family. In your region, we have serviced " + venues + " and provided " + count + " tests for fans, generating $" + count * 40 + " in shared revenue."} </p>
+                    
+                    <p style={{color:"#ffffff"}}> Thank you for considering KOACORE for your safety needs. We have worked with events ranging from Elton John to Coldplay, and we cannot wait to add you to the KOA family. In your region, we have serviced {x} and provided {y} tests for fans, generating ${y*40} in shared revenue. </p>
                 </div>
             </div>
         );
@@ -48,3 +52,4 @@ class DataDashboard extends React.Component {
 }
 
 export default DataDashboard;
+
