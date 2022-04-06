bf3e2403a0606d9ca8d3ccb76ef6c573b43f439e
condrenjustin <justin.l.condren@vanderbilt.edu>
2022-04-06 10:31:06 -0500

    added two unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+// basic smoke test
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+// make sure the expected thing is rendered -- change the expected value
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-// basic smoke test
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
 });
 
 
